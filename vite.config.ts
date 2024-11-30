01e284410ab65560aaa142293caeabb76c540076
Michael Shimeles <michaelwasihun96@gmail.com>
2024-11-30 17:46:26 -0500

    Remove vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -27,18 +27,4 @@ export default defineConfig({
       },
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "../server"),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      }
-    }
-  },
 });
