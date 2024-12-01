76d868aa2530419df8f222cf24fe80c16bb5d9b9
Michael Shimeles <michaelwasihun96@gmail.com>
2024-12-01 01:15:16 -0500

    Adding server url

--- src/hooks/useGetItems.ts
+++ src/hooks/useGetItems.ts
@@ -1,20 +1,21 @@
-// import { useQuery } from "@tanstack/react-query";
-// import { hc } from "hono/client";
-// import { AppType } from "../../../server";
+import { useQuery } from "@tanstack/react-query";
 
-// const client = hc<AppType>("/");
+export const useGetItems = () => {
+  return useQuery({
+    queryKey: ["items"],
+    queryFn: async () => {
+      // const res = await client.api.items.$get()
 
-// export const useGetItems = () => {
-//   return useQuery({
-//     queryKey: ["items"],
-//     queryFn: async () => {
-//       const res = await client.api.items.$get()
-//       const items = await res.json();
-//       return items;
-//     },
-//     // Add these options for better data handling
-//     refetchOnWindowFocus: true,
-//     refetchOnMount: true,
-//     staleTime: 1000 * 30, // Consider data stale after 30 seconcds
-//   });
-// };
+      const response = await fetch(
+        `https://hono-backend-sync-production.up.railway.app/api/items`
+      );
+
+      const items = await response.json();
+      return items;
+    },
+    // Add these options for better data handling
+    refetchOnWindowFocus: true,
+    refetchOnMount: true,
+    staleTime: 1000 * 30, // Consider data stale after 30 seconcds
+  });
+};
