ad71b6e06a5aaa1f28d116a473fd57c44df29484
Michael Shimeles <michaelwasihun96@gmail.com>
2024-12-01 01:29:27 -0500

    Update favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import LogoImage from "../assets/logo.svg";
 
 export const Route = createRootRoute({
   component: () => (
@@ -9,7 +8,7 @@ export const Route = createRootRoute({
       <div className="p-2 flex justify-between items-center w-full">
         <div className="flex gap-3 justify-center items-center">
           <Link to="/" className="p-0">
-            <img src={LogoImage} className="h-8 w-8" />
+            <img src="./logo.svg" className="h-8 w-8" />
           </Link>
           <Link to="/about" className="[&.active]:font-bold text-sm">
             About
